c5795a4930a421d0b288225db67c4a417db3a783
JFaithTrust <jahongirsolijoniy@gmail.com>
2024-04-12 20:24:53 +0500

    first commit

---
+++ lib/mongoose.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if(!process.env.MONGODB_URL) return console.log('Missing MongoDB URL');
+    if(isConnected) return console.log("MongoDB connection already established");
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+
+        isConnected = true
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
